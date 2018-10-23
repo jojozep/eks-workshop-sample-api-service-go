159bbcc7c63502cc4badf34bb7ee54e8f9ff7d1e
jojozep <37456739+jojozep@users.noreply.github.com>
2018-10-23 14:07:41 -0400

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello World War 3 - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
